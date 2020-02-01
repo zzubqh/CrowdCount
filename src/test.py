4d2e0dbaa2d9ce6e9119e118d8f766f8e8d4c15c
zzubqh <zzubqh@gmail.com>
2020-02-01 10:52:58 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -39,38 +39,6 @@ def parse_params():
     return args_
 
 
-def get_samples_malldataset(num):
-    """
-    获取测试图片
-    :return:
-    """
-
-    def get_annotation():
-        """
-        读取2000个图片的注解，得到 每个图片的人数 和 每章图片的所有人坐标
-        Annotation按照图片命名顺序
-        :return:
-        """
-        mat_annotation = sio.loadmat(os.path.join(MALL_DataPath, 'mall_gt.mat'))
-        count_data, position_data = mat_annotation['count'], mat_annotation['frame'][0]
-        return count_data, position_data
-
-    counts_true, _ = get_annotation()
-    datasize = len(glob.glob(MALL_DataPath + r'\frames\*'))
-    # 从验证集选取测试图片
-    samples_index = random.sample([i for i in range(int(0.8 * datasize), datasize)], num)
-    samples = [glob.glob(MALL_DataPath + r'\frames\*')[i] for i in samples_index]
-    images = []
-    counts = []
-    for i in range(num):
-        filename = samples[i]
-        img = cv2.resize(cv2.imread(filename), (224, 224)) / 255.
-        img = np.expand_dims(img, axis=0)
-        images.append(img)
-        counts.append(counts_true[samples_index[i]])
-    return images, counts
-
-
 def get_samples_crowdataset(num):
     import random
 
@@ -91,37 +59,6 @@ def get_samples_crowdataset(num):
     return images, counts
 
 
-def get_samples_shanghaitech(num):
-    """
-    获取测试图片
-    :return:
-    """
-
-    def get_annotation(index):
-        """
-        读取2000个图片的注解，得到 每个图片的人数 和 每章图片的所有人坐标
-        Annotation按照图片命名顺序
-        :return:
-        """
-        mat_data = sio.loadmat(Shanghaitech_DataPath + r'\part_A\test_data\ground_truth\GT_IMG_{}.mat'.format(index))
-        position_data, count_data = mat_data['image_info'][0][0][0][0][0], mat_data['image_info'][0][0][0][0][1][0][0]
-        return count_data, position_data
-
-    datasize = len(glob.glob(Shanghaitech_DataPath + r'\part_A\test_data\images\*.jpg'))
-    # 从验证集选取测试图片
-    samples_index = random.sample([i for i in range(datasize)], num)
-    samples = [Shanghaitech_DataPath + r'\part_A\test_data\images\IMG_{0}.jpg'.format(i) for i in samples_index]
-    images = []
-    counts = []
-    for i in range(num):
-        filename = samples[i]
-        img = cv2.resize(cv2.imread(filename), (224, 224)) / 255.
-        img = np.expand_dims(img, axis=0)
-        images.append(img)
-        counts.append(get_annotation(samples_index[i])[0])
-    return images, counts
-
-
 def plot_sample(raw_images, maps, counts, true_counts):
     plt.figure(figsize=(15, 9))
     for i in range(len(maps)):
@@ -132,7 +69,7 @@ def plot_sample(raw_images, maps, counts, true_counts):
         plt.imshow(maps[i][0])
         plt.title('people pred num {}'.format(counts[i]))
     plt.show()
-    # plt.savefig(os.path.join(RESULT_PATH, 'rst.png'))
+    plt.savefig(os.path.join(RESULT_PATH, 'res.png'))
 
 
 def save_result(raw_images, maps, counts, args_, true_counts):
@@ -159,16 +96,16 @@ def test(args_):
         # print(counts)
         save_result(samples, maps, counts, args_, true_counts)
     else:
-        print("Sorry, cannot find model file in root_path/models/, please download my model or train your model")
+        print("please download model frist!")
 
 
 def predict():
     import glob
     from tqdm import tqdm
 
-    data_dir = r'E:\code\kesci\crowdcount\dataset\test'
-    VGG_Model = r'E:\code\kesci\crowdcount\src\MSCNN-master\models\vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
-    Dense_Model = r'E:\code\kesci\crowdcount\src\MSCNN-master\models\dense_model_weights.h5'
+    data_dir = r'crowdcount/dataset/test'
+    VGG_Model = r'crowdcount/src/models/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+    Dense_Model = r'crowdcount/src/models/dense_model_weights.h5'
     Mscnn_Model = os.path.join(MODEL_PATH, 'mscnn_model_weights.h5')
 
     images = glob.glob(data_dir + r'\*.jpg')
@@ -206,7 +143,7 @@ def predict():
             print(img_name)
             res.append([os.path.split(img_name)[1], -1])
 
-    with open(r'E:\code\kesci\crowdcount\result\dense_res.csv', 'w') as sw:
+    with open(r'crowdcount/result/dense_res.csv', 'w') as sw:
         for item in res:
             sw.write('{0},{1}\n'.format(item[0], item[1]))
 
@@ -219,6 +156,6 @@ def imopen(im_path):
 
 
 if __name__ == '__main__':
-    predict()
-    # args = parse_params()
-    # test(args)
+    # predict()
+    args = parse_params()
+    test(args)
